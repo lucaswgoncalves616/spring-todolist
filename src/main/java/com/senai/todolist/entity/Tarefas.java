e0daf202beed3eb2c668f27fd9b1370a1be75f48
Lucas Wendell <lucas.wendell.98@gmail.com>
2025-09-17 18:55:21 -0300

    feat: basic spring application

---
+++ src/main/java/com/senai/todolist/entity/Tarefas.java
@@ -0,0 +1,14 @@
+package com.senai.todolist.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tbl_tarefas")
+public class Tarefas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String email;
+    private String senha;
+}
